b954678217880138c69e6546b90f8c6b7121f12a
João Pedro C. Araujo <124836740+joaopedroc-araujo@users.noreply.github.com>
2023-05-04 16:06:17 -0300

    Adiciona os componentes

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Title from './Title';
+
+class SolarSystem extends Component {
+  render() {
+    return (
+      <>
+        <div data-testid="solar-system" />
+        <Title headline="Planetas" />
+      </>
+    );
+  }
+}
+
+export default SolarSystem;
