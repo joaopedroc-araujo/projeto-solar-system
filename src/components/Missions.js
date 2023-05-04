a3eace1fd1b44d7603a96539b722ede789cb793c
costaaraujojoaopedro@gmail.com <costaaraujojoaopedro@gmail.com>
2023-05-04 16:55:51 -0300

    FEAT: renderiza as missões espaciais na tela

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends Component {
   render() {
     return (
-      <div data-testid="missions">
+      <>
+        <div data-testid="missions" />
         <Title headline="Missões" />
-      </div>
+        {missions.map((mission) => (
+          <MissionCard
+            key={ mission.name }
+            name={ mission.name }
+            year={ mission.year }
+            country={ mission.country }
+            destination={ mission.destination }
+          />
+        ))}
+      </>
     );
   }
 }
