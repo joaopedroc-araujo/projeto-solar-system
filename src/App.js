21d8ab7bc40aedc5426f2395d3f00f16c5dc243e
costaaraujojoaopedro@gmail.com <costaaraujojoaopedro@gmail.com>
2023-05-04 16:31:41 -0300

    FEAT: implementa componente Missions

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import PlanetCard from './components/PlanetCard';
 import SolarSystem from './components/SolarSystem';
 import Title from './components/Title';
+import Missions from './components/Missions';
 
 class App extends React.Component {
   render() {
@@ -11,6 +12,7 @@ class App extends React.Component {
         <p>Sistema Solar</p>
         <Header />
         <SolarSystem />
+        <Missions />
         <Title headline="" />
         <PlanetCard />
       </>
